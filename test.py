d1761da323eced6664a7d2aa620376f6742e3086
Jozef Knaperek <jknaperek@gmail.com>
2011-10-21 20:56:31 +0200

    Opravene handlenie exception v test.py

--- test.py
+++ test.py
@@ -2,7 +2,6 @@
 
 import sys
 import switch
-import pcapo
 import time
 
 dev_list = [b'eth0', b'eth1']
@@ -10,8 +9,8 @@ dev_list = [b'eth0', b'eth1']
 
 try:
 	s = switch.Switch(dev_list)
-except pcapo.PcapDeviceException as pcap_exception:
-	print('Chyba:', pcap_exception)
+except switch.SwitchException as switch_exception:
+	print('Chyba:', switch_exception)
 	sys.exit(1)
 
 print('Zaciatok hlavnej slucky')
